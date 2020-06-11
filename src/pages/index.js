9affadd25ec11b299b8bd797077585e129beb507
sai <kskprasad96@gmail.com>
2020-06-11 23:47:30 +0530

    Remove theme default page-2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 import { grretByTimeOfDay } from "../helpers/utils";
 
@@ -11,11 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>{grretByTimeOfDay()}</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
   </Layout>
 );
 
