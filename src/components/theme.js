841746339e0a62bef24a5fd03d8067927c06b135
sai <kskprasad96@gmail.com>
2020-06-11 15:19:20 +0530

    Persist theme in local storage

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,24 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { FaSun, FaMoon, FaCog, FaGlobeAmericas } from "react-icons/fa";
+
 const THEMES = Object.freeze({
-  dark: "theme-dark",
-  light: "theme-light",
-  system: "theme-system",
-  auto: "theme-auto",
+  dark: "dark",
+  light: "light",
+  system: "system",
+  auto: "auto",
 });
+const THEME_PREFERENCE_KEY = 'theme-preference';
+
 function Theme() {
   const [theme, setTheme] = useState(THEMES.auto);
 
   useEffect(() => {
-    let themeType = theme;
-    if (themeType === THEMES.auto) {
+    const themeFromLocalStorage = localStorage.getItem(THEME_PREFERENCE_KEY);
+
+    if (!!themeFromLocalStorage && themeFromLocalStorage !== THEMES.auto) {
+      setTheme(themeFromLocalStorage)
+    }
+  }, [])
+
+  useEffect(() => {
+    switchTheme(theme)
+  }, [theme]);
+
+  const switchTheme = (themePreference) => {
+    let themeToApply = themePreference;
+
+    if (themePreference === THEMES.auto) {
       const hourOfTheDay = new Date().getHours();
       // Light mode from 7am to 6:59pm
-      themeType =
+      themeToApply =
         hourOfTheDay > 6 && hourOfTheDay < 19 ? THEMES.light : THEMES.dark;
     }
-    document.body.setAttribute("id", themeType);
-  }, [theme]);
+
+    document.body.setAttribute("data-theme", themePreference);
+    document.body.setAttribute("id", `theme-${themeToApply}`);
+    localStorage.setItem(THEME_PREFERENCE_KEY, themePreference);
+  }
 
   return (
     <div className="theme-switch-wrapper">
