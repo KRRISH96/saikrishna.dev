5083963e4cf66796c18cc6e9a17670b1c006420c
sai <kskprasad96@gmail.com>
2020-06-11 21:21:07 +0530

    Add title for theme options

--- src/components/theme.js
+++ src/components/theme.js
@@ -38,22 +38,33 @@ function Theme() {
     document.body.setAttribute("data-theme", themePreference);
     document.body.setAttribute("id", `theme-${themeToApply}`);
     localStorage.setItem(THEME_PREFERENCE_KEY, themePreference);
-  }
+  };
 
-  const ThemeIcon = ({children, themePreference}) => <span className={`theme-icon ${theme === themePreference ? 'active' : ''}`} onClick={() => setTheme(themePreference)}>{children}</span>
+  const ThemeIcon = ({ children, themePreference, ...props }) => (
+    <span
+      className={`theme-icon ${theme === themePreference ? "active" : ""}`}
+      onClick={() => setTheme(themePreference)}
+      {...props}
+    >
+      {children}
+    </span>
+  );
 
   return (
     <div className="theme-switch-wrapper">
-      <ThemeIcon themePreference={THEMES.light}>
+      <ThemeIcon themePreference={THEMES.light} title="Light Theme">
         <FaSun />
       </ThemeIcon>
-      <ThemeIcon themePreference={THEMES.dark}>
+      <ThemeIcon themePreference={THEMES.dark} title="Dark Theme">
         <FaMoon />
       </ThemeIcon>
-      <ThemeIcon themePreference={THEMES.system}>
+      <ThemeIcon themePreference={THEMES.system} title="System Default Theme">
         <FaCog />
       </ThemeIcon>
-      <ThemeIcon themePreference={THEMES.auto}>
+      <ThemeIcon
+        themePreference={THEMES.auto}
+        title="Light during Day(7am-7pm) and Dark during Night"
+      >
         <FaGlobeAmericas />
       </ThemeIcon>
     </div>
