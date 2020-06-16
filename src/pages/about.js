18d6400ee444d3332eff34aa2b9242939a57832d
sai <kskprasad96@gmail.com>
2020-06-16 23:17:05 +0530

    Add Profile Image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,38 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import Image from "../components/image";
 
-const AboutPage = () => (
-  <Layout>
-    <SEO title="About" />
-    <div>
-      {/* <Image /> */}
-      <p>First Para</p>
-      <p>Second Para</p>
-      <div>Links</div>
-    </div>
-  </Layout>
-);
+const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      profileImage: file(relativePath: { eq: "profile_picture.jpg" }) {
+        childImageSharp {
+          fixed(width: 200, height: 200) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <Layout>
+      <SEO title="About" />
+      <div className="about-page">
+      <div className="profile-image-container">
+        <Img
+          fixed={data.profileImage.childImageSharp.fixed}
+          alt="Sai's Profile Picture"
+          className="profile-image"
+        />
+      </div>
+        <p>First Para</p>
+        <p>Second Para</p>
+        <div>Links</div>
+      </div>
+    </Layout>
+  );
+};
 
 export default AboutPage;
