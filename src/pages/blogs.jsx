b67c7636114de2f6e369ad02ad6bb11fe9551cbc
sai <kskprasad96@gmail.com>
2020-07-16 23:02:44 +0530

    WIP: add blogs page

---
+++ src/pages/blogs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const BlogsPage = () => {
+  return (
+    <Layout>
+      <SEO title="Blogs" />
+      <div className="blogs-page">
+        <h1 className="page-title">Blogs</h1>
+        <div className="blogs-container">
+          BLOGS LIST HERE
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default BlogsPage;
