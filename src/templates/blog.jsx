8789fd135a5c5871c7dbfbcc97ef1c9a9c7225de
sai <kskprasad96@gmail.com>
2020-07-20 22:36:40 +0530

    add tags

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Tags from '../components/tags';
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.markdownRemark;
@@ -15,43 +16,42 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
       />
       <div className="blog-post-page">
         <h1 className="page-title">Blogs</h1>
-        <div className="blog-post-container">
-          <article>
-            <header>
-              <h1>{post.frontmatter.title}</h1>
-              <p>{post.frontmatter.date}</p>
-              <p>{post.fields.readingTime.text}</p>
-            </header>
-            <section dangerouslySetInnerHTML={{ __html: post.html }} />
-            <hr />
-          </article>
+        <article className="blog-post-container">
+          <header className="blog-post__header">
+            <h1>{post.frontmatter.title}</h1>
+            <p>{post.frontmatter.date}</p>
+            <p>{post.fields.readingTime.text}</p>
+            <Tags tags={post.frontmatter.tags} />
+          </header>
+          <section className="blog-post__body" dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr />
+        </article>
 
-          <nav className="card other-blog-posts">
-            Other Blog Posts
-            <ul>
-              <li>
-                {previous && (
-                  <Link to={previous.frontmatter.path} rel="prev">
-                    <span role="img" aria-label="hand-with-pen">
-                      {"✍🏻 "}
-                    </span>
-                    {previous.frontmatter.title}
-                  </Link>
-                )}
-              </li>
-              <li>
-                {next && (
-                  <Link to={next.frontmatter.path} rel="next">
-                    <span role="img" aria-label="hand-with-pen">
-                      {"✍🏻 "}
-                    </span>
-                    {next.frontmatter.title}
-                  </Link>
-                )}
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <nav className="card other-blog-posts">
+          Other Blog Posts
+          <ul>
+            <li>
+              {previous && (
+                <Link to={previous.frontmatter.path} rel="prev">
+                  <span role="img" aria-label="hand-with-pen">
+                    {"✍🏻 "}
+                  </span>
+                  {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.frontmatter.path} rel="next">
+                  <span role="img" aria-label="hand-with-pen">
+                    {"✍🏻 "}
+                  </span>
+                  {next.frontmatter.title}
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
       </div>
     </Layout>
   );
@@ -70,6 +70,7 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         description
         keywords
+        tags
       }
       fields {
         slug
