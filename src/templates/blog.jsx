33bd79a3a8032eda3a666f19bdd1736b68bc165e
sai <kskprasad96@gmail.com>
2020-07-20 22:11:12 +0530

    style other blogs sections

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -13,35 +13,46 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
       />
-      <article>
-        <header>
-          <h1>{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
-          <p>{post.fields.readingTime.text}</p>
-        </header>
-        <section dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr />
-      </article>
+      <div className="blog-post-page">
+        <h1 className="page-title">Blogs</h1>
+        <div className="blog-post-container">
+          <article>
+            <header>
+              <h1>{post.frontmatter.title}</h1>
+              <p>{post.frontmatter.date}</p>
+              <p>{post.fields.readingTime.text}</p>
+            </header>
+            <section dangerouslySetInnerHTML={{ __html: post.html }} />
+            <hr />
+          </article>
 
-      <nav>
-        Other Blog Posts
-        <ul>
-          <li>
-            {previous && (
-              <Link to={previous.frontmatter.path} rel="prev">
-                {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.frontmatter.path} rel="next">
-                {next.frontmatter.title}
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
+          <nav className="card other-blog-posts">
+            Other Blog Posts
+            <ul>
+              <li>
+                {previous && (
+                  <Link to={previous.frontmatter.path} rel="prev">
+                    <span role="img" aria-label="hand-with-pen">
+                      {"✍🏻 "}
+                    </span>
+                    {previous.frontmatter.title}
+                  </Link>
+                )}
+              </li>
+              <li>
+                {next && (
+                  <Link to={next.frontmatter.path} rel="next">
+                    <span role="img" aria-label="hand-with-pen">
+                      {"✍🏻 "}
+                    </span>
+                    {next.frontmatter.title}
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </Layout>
   );
 };
