45515e83f16ece1df74cc8e96abee37d17c6a717
sai <kskprasad96@gmail.com>
2020-07-23 23:45:28 +0530

    contact skeleton change and style

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./index.scss";
 
 const ContactForm = () => {
-  return <form>FOrm here</form>;
+  return (
+    <form className="contact-form">
+      <div className="form-elements-container">
+        <label htmlFor="name" className="form-label">
+          <span>Name</span>
+        </label>
+        <input id="name" className="form-input" />
+
+        <label htmlFor="email" className="form-label">
+          <span>Email</span>
+        </label>
+        <input id="email" className="form-input" />
+        <label htmlFor="comment" className="form-label">
+          <span>Whats Up?</span>
+        </label>
+        <textarea id="comment" className="form-input" />
+      </div>
+    </form>
+  );
 };
 
 export default ContactForm;
