615d815c977043dc6e33679f20bb7391b262aead
sai <kskprasad96@gmail.com>
2020-07-25 20:38:36 +0530

    ditched netlify and integrate formspree

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -1,48 +1,14 @@
 import React from "react";
-import { navigateTo } from "gatsby";
 import "./index.scss";
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
-
 const ContactForm = () => {
-  const [formData, setFormData] = React.useState({});
-
-  const handleChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/contact?no-cache=1", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...formData,
-      }),
-    })
-      .then(() => navigateTo(form.getAttribute("action")))
-      .catch(error => console.log(error));
-  };
-
   return (
     <form
       name="contact"
-      action="/thank-you"
+      action="https://formspree.io/xqkywygr"
       method="POST"
-      netlify-honeypot="bot-field"
-      data-netlify-recaptcha="true"
       className="contact-form"
-      data-netlify="true"
-      onSubmit={handleSubmit}
     >
-      <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value="contact" />
       <label htmlFor="name" className="form-label">
         Name
         <br />
@@ -52,7 +18,6 @@ const ContactForm = () => {
           name="name"
           className="form-input"
           placeholder="John Doe"
-          onChange={handleChange}
           required
         />
       </label>
@@ -62,10 +27,9 @@ const ContactForm = () => {
         <input
           id="email"
           type="email"
-          name="email"
+          name="_replyto"
           className="form-input"
           placeholder="john_doe@example.com"
-          onChange={handleChange}
           required
         />
       </label>
@@ -78,7 +42,6 @@ const ContactForm = () => {
           name="message"
           className="form-input"
           placeholder="What's Up?"
-          onChange={handleChange}
           required
         />
       </label>
