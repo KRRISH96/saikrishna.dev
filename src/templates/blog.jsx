748e3b1ea9d2e1190c031facc44da431ddf96407
sai <kskprasad96@gmail.com>
2020-07-26 02:13:35 +0530

    Add blog post meta data

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -5,29 +5,47 @@ import SEO from "../components/seo";
 import BlogPostMeta from "../components/presentational/blogPostMeta";
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
-  const post = data.markdownRemark;
+  const { frontmatter, fields, excerpt, html } = data.markdownRemark;
   const { previous, next } = pageContext;
+
+  const metaData = [
+    {
+      name: `keywords`,
+      content: frontmatter.keywords,
+    },
+    {
+      name: `twitter:label1`,
+      content: "Reading Time",
+    },
+    {
+      name: `twitter:data1`,
+      content: frontmatter.readingTime,
+    },
+    {
+      name: `article:published_time`,
+      content: frontmatter.date,
+    },
+  ];
 
   return (
     <Layout location={location} title={"Blogs"}>
       <SEO
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
+        title={frontmatter.title}
+        description={frontmatter.description || excerpt}
+        meta={metaData}
       />
       <div className="blog-post-page">
         <article className="blog-post-container">
           <header className="blog-post__header">
-            <h1 className="page-title blog-post__title">
-              {post.frontmatter.title}
-            </h1>
+            <h1 className="page-title blog-post__title">{frontmatter.title}</h1>
             <BlogPostMeta
-              readingTime={post.fields.readingTime}
-              date={post.frontmatter.date}
+              readingTime={fields.readingTime}
+              date={frontmatter.date}
             />
           </header>
           <section
             className="blog-post__body"
-            dangerouslySetInnerHTML={{ __html: post.html }}
+            dangerouslySetInnerHTML={{ __html: html }}
           />
           <hr />
         </article>
