2cea733ad68307518ecf83ab87af900826efc4ee
sai <kskprasad96@gmail.com>
2020-08-02 00:26:09 +0530

    remove unused import

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import { APP_ROUTES } from "../helpers/constants";
 import { Link } from "gatsby";
 
 const NotFoundPage = () => (
