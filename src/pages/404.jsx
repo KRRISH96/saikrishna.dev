615895edf426c1e378fc5dbc1ca30a8637785d13
sai <kskprasad96@gmail.com>
2020-08-03 23:25:55 +0530

    update 404 page design

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-
+import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import { Link } from "gatsby";
+import NotFoundSVG from "../components/presentational/NotFoundSVG";
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="Uh Oh! Looks like you are Lost in Space" />
-    <h1>Uh Oh! Looks Like You are Lost in Space</h1>
-    <p>
-      Let's get you back to <Link to="/">Home</Link> Planet
-    </p>
+    <SEO title="404 | Uh Oh! Looks like you are Lost in Space" />
+    <div className="not-found-page">
+      <h1 className="page-title">Uh Oh! Looks Like You are Lost in Space</h1>
+      <div className="four-o-four">
+        <span>4</span>
+        <span className="illustration">
+          <NotFoundSVG />
+        </span>
+        <span>4</span>
+      </div>
+      <p>
+        That's enough spacewalk. Let's get you back to the{" "}
+        <Link to="/">base</Link>
+      </p>
+    </div>
   </Layout>
 );
 
