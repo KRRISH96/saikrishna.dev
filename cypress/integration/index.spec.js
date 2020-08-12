0384f0041cd34f73f837e02086e636426ea343b7
sai <kskprasad96@gmail.com>
2020-08-12 23:04:11 +0530

    use findByText, getByText is deprecated

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -5,22 +5,22 @@ describe("Home Page", () => {
   });
 
   it("Can do About Page Navigation", () => {
-    cy.getByText("About").click().waitForRouteChange();
+    cy.findByText("about").click().waitForRouteChange();
     cy.url().should("equal", `${window.location.origin}/about`);
   });
 
   it("Can do Projects Page Navigation", () => {
-    cy.getByText("Projects").click().waitForRouteChange();
+    cy.findByText("projects").click().waitForRouteChange();
     cy.url().should("equal", `${window.location.origin}/projects`);
   });
 
   it("Can do Blogs Page Navigation", () => {
-    cy.getByText("Blogs").click().waitForRouteChange();
+    cy.findByText("blogs").click().waitForRouteChange();
     cy.url().should("equal", `${window.location.origin}/blogs`);
   });
 
   it("Can do Contact Page Navigation", () => {
-    cy.getByText("Contact").click().waitForRouteChange();
+    cy.findByText("contact").click().waitForRouteChange();
     cy.url().should("equal", `${window.location.origin}/contact`);
   });
 });
