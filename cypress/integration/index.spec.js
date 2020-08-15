6e18fa5babf675a1066fa4329d3c0081ed42d9f3
sai <kskprasad96@gmail.com>
2020-08-15 23:21:50 +0530

    add husky for precommit hooks - format before commit and test before push

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -4,22 +4,22 @@ describe("Home Page", () => {
   });
 
   it("Can do About Page Navigation", () => {
-    cy.get('[data-cy=about]').click().waitForRouteChange();
+    cy.get("[data-cy=about]").click().waitForRouteChange();
     cy.url().should("equal", `${window.location.origin}/about`);
   });
 
   it("Can do Projects Page Navigation", () => {
-    cy.get('[data-cy=projects]').click().waitForRouteChange();
+    cy.get("[data-cy=projects]").click().waitForRouteChange();
     cy.url().should("equal", `${window.location.origin}/projects`);
   });
 
   it("Can do Blogs Page Navigation", () => {
-    cy.get('[data-cy=blogs]').click().waitForRouteChange();
+    cy.get("[data-cy=blogs]").click().waitForRouteChange();
     cy.url().should("equal", `${window.location.origin}/blogs`);
   });
 
   it("Can do Contact Page Navigation", () => {
-    cy.get('[data-cy=contact]').click().waitForRouteChange();
+    cy.get("[data-cy=contact]").click().waitForRouteChange();
     cy.url().should("equal", `${window.location.origin}/contact`);
   });
 });
