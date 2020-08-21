17a4a76a97663e76536b4a62391019592acde639
sai <kskprasad96@gmail.com>
2020-08-21 23:15:18 +0530

    add blog link in navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
-import { APP_ROUTES } from "../helpers/constants";
+import { APP_ROUTES, BLOG_SITE } from "../helpers/constants";
+import ExternalLink from "./presentational/externalLink";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const Navigation = ({ isHomePage }) => (
   <nav>
     <ul className={`navigation ${!isHomePage ? "top-nav" : ""}`}>
+      <li>
+        <small className="external-link-icon">
+          <FaExternalLinkAlt />
+        </small>
+        <ExternalLink href={BLOG_SITE}>Blog</ExternalLink>
+      </li>
       {Object.keys(APP_ROUTES).map(route => (
         <li key={route}>
           <Link to={APP_ROUTES[route]} activeClassName="active" data-cy={route}>
